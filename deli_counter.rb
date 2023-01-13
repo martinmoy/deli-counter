f676bb71081949bfb6ef7affd24a1456f5f5f3a3
Martin Moy <martinmoy1991@gmail.com>
2023-01-13 01:42:09 -0500

    commit tree d70ea4463b4c264d2c720eadc42d86f4153539df

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+
+def line(katz_deli)
+  if katz_deli == []
+  puts "The line is currently empty."
+  else
+    array_holder =[]
+    katz_deli.each.with_index(1) do |name, index|
+      array_holder.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{array_holder.join(" ")}"
+
+    #"The line is currently: "  then we need to put index then name, hence we need to split array
+    
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli == []
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift()
+  end
+
+end
